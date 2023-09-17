5e9c2d30492aaacb588f208e43188bff60c2195c
Tech MAC <techmac@Techs-MacBook-Air.local>
2023-09-17 21:13:51 +0530

    responsive working

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import consulting from "../../public/img/consulting.png";
 
 export default function Home() {
   return (
-    <main className="bg-white text-black px-10">
+    <main className="bg-white text-black px-10 md:px-20 lg:px-40">
       <section className="min-h-screen">
         <nav className="py-10 mb-12 flex justify-between">
           <h1 className="text-2xl font-burtons">Codeskul</h1>
@@ -28,12 +28,12 @@ export default function Home() {
         </nav>
 
         <div className="text-center p-10">
-          <h2 className="text-5xl py-2 text-teal-600 font-medium">
+          <h2 className="text-5xl md:text-6xl py-2 text-teal-600 font-medium">
             Sanjay Patel
           </h2>
-          <h3 className="text-2xl py-2">Full-Stack Developer</h3>
-          <p className="text-md py-5 leading-8 text-gray-800">
-            Freelancer providing services for JavaScript programming. Join me
+          <h3 className="text-2xl md:text-3xl py-2">Full-Stack Developer</h3>
+          <p className="text-md md:text-xl max-w-xl mx-auto py-5 leading-8 text-gray-800">
+            Freelancer providing services for programming and design needs. Join me
             down below and let's get cracking!
           </p>
         </div>
@@ -43,7 +43,7 @@ export default function Home() {
           <AiFillGithub />
         </div>
 
-        <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-70 h-70 mt-20 overflow-hidden">
+        <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-72 h-72 my-20 overflow-hidden">
           <Image
             src={deved}
             alt="dev"
@@ -72,7 +72,20 @@ export default function Home() {
 
         <div>
           <div className="text-center shadow-lg p-10 rounded-xl my-10">
-            <Image src={design} width={100} height={100} alt="design" />
+            <Image className="block mx-auto" src={design} width={100} height={100} alt="design" />
+            <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
+            <p className="py-2">
+              Creating elegant designs suited for your needs following core
+              design theory.
+            </p>
+            <h4 className="py-4 text-teal-600">Design tools I use</h4>
+            <p className="text-gray-800 py-1">Figma</p>
+            <p className="text-gray-800 py-1">Bootstrap 5</p>
+            <p className="text-gray-800 py-1">Tailwind CSS</p>
+          </div>
+
+          <div className="text-center shadow-lg p-10 rounded-xl my-10">
+            <Image className="block mx-auto" src={code} width={100} height={100} alt="design" />
             <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
             <p className="py-2">
               Creating elegant designs suited for your needs following core
@@ -81,6 +94,23 @@ export default function Home() {
             <h4 className="py-4 text-teal-600">Design tools I use</h4>
             <p className="text-gray-800 py-1">Figma</p>
           </div>
+          
+          <div className="text-center shadow-lg p-10 rounded-xl my-10">
+            <Image className="block mx-auto" src={consulting} width={100} height={100} alt="design" />
+            <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
+            <p className="py-2">
+              Creating elegant designs suited for your needs following core
+              design theory.
+            </p>
+            <h4 className="py-4 text-teal-600">Design tools I use</h4>
+            <p className="text-gray-800 py-1">Figma</p>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <h3 className="text-3xl py-1">Portfolio</h3>
         </div>
       </section>
     </main>
