74b519be7327fd5a855e0094ca0c20583c731ea4
Tech MAC <techmac@Techs-MacBook-Air.local>
2023-09-17 20:44:19 +0530

    about information adding

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,17 @@
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import Image from "next/image";
+import deved from "../../public/img/dev-ed-wave.png";
+import design from "../../public/img/design.png";
+import code from "../../public/img/code.png";
+import consulting from "../../public/img/consulting.png";
 
 export default function Home() {
   return (
     <main className="bg-white text-black px-10">
       <section className="min-h-screen">
         <nav className="py-10 mb-12 flex justify-between">
-          <h1 className="text-xl font-burtons">Codeskul</h1>
+          <h1 className="text-2xl font-burtons">Codeskul</h1>
           <ul className="flex text-center">
             <li>
               <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
@@ -38,8 +43,44 @@ export default function Home() {
           <AiFillGithub />
         </div>
 
+        <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-70 h-70 mt-20 overflow-hidden">
+          <Image
+            src={deved}
+            alt="dev"
+            priority={true}
+            className="aspect-square overflow-clip"
+          />
+        </div>
+      </section>
+
+      <section>
         <div>
-          <img src="./img/dev-ed-wave.png" alt="" />
+          <h3 className="text-3xl py-1">Services I Offer</h3>
+          <p className="text-md py-2 leading-8 text-gray-800">
+            Since the beginning of my journey as a full-stack developer, I've
+            done remote work for
+            <span className="text-teal-500"> agencies </span>
+            consulted for <span className="text-teal-500">startups </span>
+            and collaborated with talanted people to create digital products for
+            both business and consumer use.
+          </p>
+          <p className="text-md py-2 leading-8 text-gray-800">
+            I offer from a wide range of services, including programming and
+            teaching.
+          </p>
+        </div>
+
+        <div>
+          <div className="text-center shadow-lg p-10 rounded-xl my-10">
+            <Image src={design} width={100} height={100} alt="design" />
+            <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
+            <p className="py-2">
+              Creating elegant designs suited for your needs following core
+              design theory.
+            </p>
+            <h4 className="py-4 text-teal-600">Design tools I use</h4>
+            <p className="text-gray-800 py-1">Figma</p>
+          </div>
         </div>
       </section>
     </main>
